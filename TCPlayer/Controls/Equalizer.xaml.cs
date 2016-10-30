ed48b51e8ca887e57578a513e4dfea406dfa1fda
webmaster442 <ruzsinszki.gabor@gmail.com>
2016-10-30 08:15:22 +0100

    More equalizer work

--- TCPlayer/Controls/Equalizer.xaml.cs
+++ TCPlayer/Controls/Equalizer.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using TCPlayer.Code;
 
 namespace TCPlayer.Controls
 {
@@ -10,15 +11,19 @@ namespace TCPlayer.Controls
     {
         public delegate void EqualizerBandChange(int band, float value);
 
+        private bool _fireevent;
+
         public event EqualizerBandChange EqBandChanged;
 
         public Equalizer()
         {
             InitializeComponent();
+            _fireevent = true;
         }
 
         private void FreqChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            if (!_fireevent) return;
             var s = (sender as Slider).Name;
             int index = 0;
             switch (s)
@@ -63,6 +68,19 @@ namespace TCPlayer.Controls
 
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
+            _fireevent = false;
+            var values = Helpers.LoadEqPresets();
+            Freq32.Value = values[0];
+            Freq64.Value = values[1];
+            Freq125.Value = values[2];
+            Freq250.Value = values[3];
+            Freq500.Value = values[4];
+            Freq1k.Value = values[5];
+            Freq2k.Value = values[6];
+            Freq4k.Value = values[7];
+            Freq8k.Value = values[8];
+            Freq16k.Value = values[9];
+            _fireevent = true;
         }
     }
 }
