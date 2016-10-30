bd326cbcb3dfaa1ac95993870e0f28be200cefcf
webmaster442 <ruzsinszki.gabor@gmail.com>
2016-10-30 09:26:40 +0100

    Equalizer work

--- TCPlayer/Controls/Equalizer.xaml.cs
+++ TCPlayer/Controls/Equalizer.xaml.cs
@@ -1,4 +1,22 @@
-﻿using System.Windows;
+﻿/*
+    TC Plyer
+    Total Commander Audio Player plugin & standalone player written in C#, based on bass.dll components
+    Copyright (C) 2016 Webmaster442 aka. Ruzsinszki Gábor
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System.Windows;
 using System.Windows.Controls;
 using TCPlayer.Code;
 
@@ -81,6 +99,20 @@ namespace TCPlayer.Controls
             Freq8k.Value = values[8];
             Freq16k.Value = values[9];
             _fireevent = true;
+        }
+
+        private void BtnPresetFlat_Click(object sender, RoutedEventArgs e)
+        {
+            Freq32.Value = 0.0d;
+            Freq64.Value = 0.0d;
+            Freq125.Value = 0.0d;
+            Freq250.Value = 0.0d;
+            Freq500.Value = 0.0d;
+            Freq1k.Value = 0.0d;
+            Freq2k.Value = 0.0d;
+            Freq4k.Value = 0.0d;
+            Freq8k.Value = 0.0d;
+            Freq16k.Value = 0.0d;
         }
     }
 }
