1907f6ce1dbfb4660ecbd1340935229d88f32970
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-10-06 14:27:10 +0200

    Extenal radio bookmark possibility

--- TCPlayer/Controls/RadioStations.xaml.cs
+++ TCPlayer/Controls/RadioStations.xaml.cs
@@ -16,8 +16,11 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -31,37 +34,90 @@ namespace TCPlayer.Controls
     /// </summary>
     public partial class RadioStations : UserControl
     {
+        private string _bookmarkdir;
         private bool loaded;
+        private void BuildRadioUi(Stream stream)
+        {
+            var nodes = new List<TreeViewItem>();
+            var xs = new XmlSerializer(typeof(RadioGroup), new XmlRootAttribute("bookmarks"));
+            var data = (RadioGroup)xs.Deserialize(stream);
+            var root = data.SubGroups[0].SubGroups;
+            foreach (var g in root)
+            {
+                nodes.Add(RenderNodes(g));
+            }
+            RadioView.ItemsSource = nodes;
+        }
 
         private void FileSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             switch (FileSelector.SelectedIndex)
             {
                 case 0:
-                    LoadXML("radiotray.xml");
+                    LoadInternalXml("radiotray.xml");
                     break;
+
                 case 1:
-                    LoadXML("radiotray.hu.xml");
+                    LoadInternalXml("radiotray.hu.xml");
+                    break;
+
+                default:
+                    LoadExternalXml(Bookmarks[FileSelector.SelectedIndex]);
                     break;
             }
         }
 
-        private void LoadXML(string file)
+        private void FillBookmarkList()
+        {
+            List<string> Files = new List<string>
+            {
+                Properties.Resources.Radio_RadioTcPlayer,
+                Properties.Resources.Radio_RadioHungarian
+            };
+
+            if (Directory.Exists(_bookmarkdir))
+            {
+                var xmls = Directory.GetFiles(_bookmarkdir, "*.xml");
+                foreach (var xml in xmls)
+                {
+                    Files.Add(Path.GetFileName(xml));
+                }
+            }
+
+            Bookmarks = new ObservableCollection<string>(Files);
+            FileSelector.ItemsSource = Bookmarks;
+        }
+
+        private void LoadExternalXml(string file)
         {
             if (!loaded) return;
-            var nodes = new List<TreeViewItem>();
+            var fullpath = Path.Combine(_bookmarkdir, file);
+
+            if (File.Exists(fullpath))
+            {
+                try
+                {
+                    using (var fs = File.OpenRead(fullpath))
+                    {
+                        BuildRadioUi(fs);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    RadioView.ItemsSource = null;
+                    MessageBox.Show(ex.Message, Properties.Resources.Error_Title, MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void LoadInternalXml(string file)
+        {
+            if (!loaded) return;
             var rs = Application.GetResourceStream(new Uri("/TCPlayer;component/Lib/" + file, UriKind.Relative));
             using (var stream = rs.Stream)
             {
-                var xs = new XmlSerializer(typeof(RadioGroup), new XmlRootAttribute("bookmarks"));
-                var data = (RadioGroup)xs.Deserialize(stream);
-                var root = data.SubGroups[0].SubGroups;
-                foreach (var g in root)
-                {
-                    nodes.Add(RenderNodes(g));
-                }
+                BuildRadioUi(stream);
             }
-            RadioView.ItemsSource = nodes;
         }
 
         private void RadioView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -103,9 +159,13 @@ namespace TCPlayer.Controls
         public RadioStations()
         {
             InitializeComponent();
+            _bookmarkdir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Radio");
+            FillBookmarkList();
             loaded = true;
             FileSelector.SelectedIndex = 0;
         }
+
+        public ObservableCollection<string> Bookmarks { get; private set; }
 
         public string SelectedUrl
         {
---
+++ TCPlayer/Radio/Radio.md
@@ -0,0 +1,9 @@
+﻿# Custom Radio bookmarks
+
+TC Player supports radio station bookmarks in Radiotray format. http://radiotray.sourceforge.net/
+
+All you have to do is place the Radiotray compatible xml files into this folder and the player will read them & you will be able to select them in the player.
+
+A Custom sample XML is also provided.
+
+For Hungarian stations the player has embeded Radiotray-hu: https://github.com/gyarakilaszlo/Radiotray-hu
