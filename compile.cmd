3b80ab9771ca65277655075360a4f09d6861800e
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2019-04-20 18:52:05 +0200

    Updated project to build with VS2019

--- compile.cmd
+++ compile.cmd
@@ -1,6 +1,24 @@
-"c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" /m TotalCommanderPlugins.sln /p:Configuration=Release /p:Platform=x64
-"c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" /m TotalCommanderPlugins.sln /p:Configuration=Release /p:Platform=x86
-"c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" /m TCPlayer.sln /p:Configuration="Release"
+set vs2017="c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
+set vs2019="c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe"
+set compiler=""
+
+if exist %vs2019% (
+	set compiler=%vs2019%
+	goto compile
+)
+if exist %vs2017% (
+	set compiler=%vs2017%
+	goto compile
+)
+
+REM No compiler found exit...
+goto exit
+
+
+:compile
+%compiler% /m TotalCommanderPlugins.sln /p:Configuration=Release /p:Platform=x64
+%compiler% /m TotalCommanderPlugins.sln /p:Configuration=Release /p:Platform=x86
+%compiler% /m TCPlayer.sln /p:Configuration="Release"
 
 cd bin\Release
 del *.pdb
@@ -8,3 +26,5 @@ del *.exp
 del *.iobj
 del *.lib
 del *.ipdb
+
+:exit
