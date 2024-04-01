20635e42d68754ff2c8c44371210bcf7e5747003
webmaster442 <ruzsinszki.gabor@gmail.com>
2024-04-01 20:16:05 +0200

    Layout

---
+++ TcPlayer/Controls/ExtendedControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TcPlayer.Controls
+{
+    internal class ExtendedControl: Control
+    {
+        protected T GetTemplateChild<T>(string name) where T : DependencyObject
+        {
+            if (GetTemplateChild(name) is T casted)
+            {
+                return casted;
+            }
+            throw new InvalidOperationException($"{name} not found or type mismatch");
+        }
+    }
+}
---
+++ TcPlayer/Controls/WindowControls.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace TcPlayer.Controls
+{
+
+    internal class WindowControls : ExtendedControl
+    {
+        public Window ControlledWindow
+        {
+            get { return (Window)GetValue(ControlledWindowProperty); }
+            set { SetValue(ControlledWindowProperty, value); }
+        }
+
+        public static readonly DependencyProperty ControlledWindowProperty =
+            DependencyProperty.Register("ControlledWindow", typeof(Window), typeof(WindowControls), new PropertyMetadata(null));
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            GetTemplateChild<Button>("PART_Minimize").Click += OnMinimize;
+            GetTemplateChild<Button>("PART_Maximize").Click += OnMaximize;
+            GetTemplateChild<Button>("PART_Close").Click += OnClose;
+            GetTemplateChild<Border>("PART_Border").MouseLeftButtonDown += OnWindowMove;
+            GetTemplateChild<ToggleButton>("PART_Pin").Click += OnPin;
+        }
+
+        private void OnPin(object sender, RoutedEventArgs e)
+        {
+            if (sender is ToggleButton button
+                && ControlledWindow != null
+                && button.IsChecked.HasValue)
+            {
+                ControlledWindow.Topmost = (bool)button.IsChecked;
+            }
+        }
+
+        private void OnWindowMove(object sender, MouseButtonEventArgs e)
+            => ControlledWindow?.DragMove();
+
+        private void OnClose(object sender, RoutedEventArgs e)
+            => ControlledWindow?.Close();
+
+        private void OnMaximize(object sender, RoutedEventArgs e)
+        {
+            if (ControlledWindow == null)
+                return;
+
+            if (ControlledWindow.WindowState == WindowState.Normal)
+                ControlledWindow.WindowState = WindowState.Maximized;
+            else
+                ControlledWindow.WindowState = WindowState.Normal;
+        }
+
+        private void OnMinimize(object sender, RoutedEventArgs e)
+            => ControlledWindow.WindowState = WindowState.Minimized;
+    }
+}
