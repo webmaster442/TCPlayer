e005971626d2ff5753d8edeb05628b5937dd03b2
webmaster442 <ruzsinszki.gabor@gmail.com>
2024-04-01 17:17:38 +0200

    V3.0 Skeleton

--- TcPlayer/App.xaml.cs
+++ TcPlayer/App.xaml.cs
@@ -9,15 +9,6 @@ namespace TcPlayer
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            DispatcherUnhandledException += App_DispatcherUnhandledException;
-        }
-
-        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
-        {
-            var ex = e.Exception;
-        }
     }
 
 }
